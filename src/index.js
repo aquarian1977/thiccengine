1e00894de3ed824e15ec6b11355cbf0721068ff0
aquarian1977 <aquarian1977@local.host>
2026-08-31 20:46:29 -0700

    Introduce HSV color, display 360º of lines with rotating HSV color in center of frame, and fix bug with line drawing where non-ints were getting fed into drawPixel.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import ThiccEngine, {ColorRGB, FrameBuffer} from "./thiccengine.js"
+import ThiccEngine, {ColorRGB, ColorHSV, FrameBuffer} from "./thiccengine.js"
 import WebRenderTarget from "./renderer.js"
 import {Tri, Vector3} from "./geometry.js"
 
 const FRAME_WIDTH_PIXELS = 300
 const FRAME_HEIGHT_PIXELS = 300
-const COLOR_DARK_TEAL = new ColorRGB(0, 63, 63)
-const COLOR_LIME = new ColorRGB(0, 255, 0)
 const TARGET_FPS = 30
-const ANGLE_PER_FRAME = 2 * Math.PI / 180
 
 let frameBuffer, renderTarget, scene
 
@@ -26,35 +23,12 @@ function init () {
 
 function renderFrame () {
     ThiccEngine.fill(frameBuffer, new ColorRGB(0, 0, 0))
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 150, 150, new ColorRGB(255, 255, 255))
-    renderTarget.display(frameBuffer)
-
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 250, 150, new ColorRGB(255, 0, 0))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 221, 221, new ColorRGB(0, 0, 255))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 221, 79, new ColorRGB(0, 0, 255))
-    renderTarget.display(frameBuffer)
-
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 50, 150, new ColorRGB(255, 0, 0))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 79, 221, new ColorRGB(0, 0, 255))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 79, 79, new ColorRGB(0, 0, 255))
-    renderTarget.display(frameBuffer)
-
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 150, 250, new ColorRGB(255, 0, 0))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 188, 242, new ColorRGB(0, 255, 0))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 112, 242, new ColorRGB(0, 255, 0))
-    renderTarget.display(frameBuffer)
-
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 150, 50, new ColorRGB(255, 0, 0))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 188, 58, new ColorRGB(0, 255, 0))
-    renderTarget.display(frameBuffer)
-    ThiccEngine.drawLine(frameBuffer, 150, 150, 112, 58, new ColorRGB(0, 255, 0))
+    for (let thetaDegrees = 0; thetaDegrees < 360; thetaDegrees += 5) {
+        const thetaRadians = (thetaDegrees / 360) * 2 * Math.PI
+        const deltaX = Math.cos(thetaRadians) * 100
+        const deltaY = Math.sin(thetaRadians) * 100
+        ThiccEngine.drawLine(frameBuffer, 150, 150, 150 + deltaX, 150 + deltaY, new ColorHSV(thetaDegrees, 1, 1))
+    }
     renderTarget.display(frameBuffer)
 }
 
--- src/thiccengine.js
+++ src/thiccengine.js
@@ -1,13 +1,38 @@
 class ColorRGB extends Uint8ClampedArray {
-    static BYTE_LENGTH = 4
     constructor (r = 0, g = 0, b = 0, a = 255) {
         super([r, g, b, a])
     }
 }
 
+class ColorHSV extends ColorRGB {
+    constructor (rawH = 0, s = 1, v = 1) {
+        // Taken from https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+        // Conversion decides primary hue then mixes other colors to hit saturation and value
+        const h = rawH % 360 // Clamp h to below 360
+        const c = s * v
+        const m = v - c
+        const x = c * (1 - Math.abs(((h/60) % 2) - 1))
+        let rprime = 0, gprime = 0, bprime = 0
+        if (h >= 0 && h < 60) {
+            rprime = c, gprime = x, bprime = 0
+        } else if (h >= 60 && h < 120) {
+            rprime = x, gprime = c, bprime = 0
+        } else if (h >= 120 && h < 180) {
+            rprime = 0, gprime = c, bprime = x
+        } else if (h >= 180 && h < 240) {
+            rprime = 0, gprime = x, bprime = c
+        } else if (h >= 240 && h < 300) {
+            rprime = x, gprime = 0, bprime = c
+        } else if (h >= 300 && h < 360) {
+            rprime = c, gprime = 0, bprime = x
+        }
+        super((rprime + m) * 255, (gprime + m) * 255, (bprime + m) * 255)
+    }
+}
+
 class FrameBuffer {
     constructor (width = 320, height = 200) {
-        this.bytesPerPixel = ColorRGB.BYTE_LENGTH
+        this.bytesPerPixel = 4
         this.data = new Uint8ClampedArray(width * height * this.bytesPerPixel)
         this.width = width
         this.height = height
@@ -40,7 +65,7 @@ function drawLine (buffer, x1, y1, x2, y2, color) {
         const gradient = deltaY/deltaX
         for (let x = initX; x <= finalX; x += 1) {
             let y = (gradient * (x - initX)) + initY
-            drawPixel(buffer, x, Math.round(y), color)
+            drawPixel(buffer, Math.round(x), Math.round(y), color)
         }
     } else if (Math.abs(y2 - y1) > 0 && Math.abs((x2 - x1)/(y2 - y1)) <= 1) {
         const increasingY = y2 > y1
@@ -53,7 +78,7 @@ function drawLine (buffer, x1, y1, x2, y2, color) {
         const gradient = deltaX/deltaY
         for (let y = initY; y <= finalY; y += 1) {
             let x = (gradient * (y - initY)) + initX
-            drawPixel(buffer, Math.round(x), y, color)
+            drawPixel(buffer, Math.round(x), Math.round(y), color)
         }
     }
 }
@@ -75,5 +100,5 @@ function drawScene (buffer, scene, color) {
     })
 }
 
-export {ColorRGB, FrameBuffer}
+export {ColorRGB, ColorHSV, FrameBuffer}
 export default {drawScene, drawPixel, drawLine, drawRect, fill}
